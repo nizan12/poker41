5e0cb2c254aec99b2aef3185529e18c3ca6702ae
nizan12 <nizan1258@gmail.com>
2026-06-30 22:49:24 +0700

    update

--- src/features/scene/components/BgmPlayer.tsx
+++ src/features/scene/components/BgmPlayer.tsx
@@ -5,7 +5,8 @@ import { useUIStore } from '@/features/game/stores/uiStore';
 
 export const BGM_PLAYLIST = [
   { name: 'Obh Combi Sachet', url: '/bgm.mp3' },
-  { name: 'Mejikuhibiniu', url: '/bgm1.mp3' }
+  { name: 'Mejikuhibiniu', url: '/bgm1.mp3' },
+  { name: 'Mejikuhibiniu', url: '/bgm2.mp3' }
 ];
 
 export function BgmPlayer() {
